93bc33caa5edcde4325fce2184096e6c06ace3b1
jorshi <jordieshier@gmail.com>
2022-12-10 00:14:55 +0000

    Boiler plate script entry

--- python/dataset_preprocess.py
+++ python/dataset_preprocess.py
@@ -5,11 +5,28 @@ Segments audio files so all are less than 60 seconds.
 """
 import logging
 import os
+import sys
+import argparse
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 
+# PETs to include
+tech_types = ["Glissando", "Portamento", "Acciacatura"]
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-i', '--in', default="./CBFdataset", help="Path to CBF Dataset")
+    parser.add_argument('-o', '--out', default="./CBFdataset_PETS", help="Output folder for PETs Dataset")
+
+    args = parser.parse_args(arguments)
+    print(args)
+
 
 if __name__ == "__main__":
     log.info("Dataset Preprocessing")
+    sys.exit(main(sys.argv[1:]))
