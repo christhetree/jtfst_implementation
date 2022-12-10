8b50ab6340e7ffac238d30969d43d7e8032f738b
jorshi <jordieshier@gmail.com>
2022-12-10 00:25:25 +0000

    get annotation files for PETs

--- python/dataset_preprocess.py
+++ python/dataset_preprocess.py
@@ -7,6 +7,7 @@ import logging
 import os
 import sys
 import argparse
+from pathlib import Path
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -15,16 +16,42 @@ log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 # PETs to include
 tech_types = ["Glissando", "Portamento", "Acciacatura"]
 
+def get_annotation_files(wavfiles: list):
+    """
+    Load all the annotation files for each wavfile
+    Check to make sure the wavfile contains annotations
+    for a technique type that we are considering.
+    """
+    pets = []
+    non_pets = []
+    for w in wavfiles:
+        # Get all the annotation files for the wavfile -- there may be more than one
+        annotations = list(Path(w.parent).rglob(f"{w.stem}*.csv"))
+        annos_for_wav = []
+        for a in annotations:
+            tech = a.stem.split("_")[-1]
+            if tech in tech_types:
+                annos_for_wav.append(a)
+
+        if len(annos_for_wav) > 0:
+            pets.append((w, annos_for_wav))
+
+    return pets
 
 def main(arguments):
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-i', '--in', default="./CBFdataset", help="Path to CBF Dataset")
+    parser.add_argument('-d', '--dataset', default="./CBFdataset", help="Path to CBF Dataset")
     parser.add_argument('-o', '--out', default="./CBFdataset_PETS", help="Output folder for PETs Dataset")
 
     args = parser.parse_args(arguments)
-    print(args)
+
+    # Wav files and annotation files for PETs
+    wavfiles = list(Path(args.dataset[0]).rglob("*.wav"))
+    annotation_files = get_annotation_files(wavfiles)
+
+    print(annotation_files)
 
 
 if __name__ == "__main__":
