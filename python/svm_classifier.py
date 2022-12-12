1b1d85aae02821bce90b4c795f1aa73be9c67ef0
jorshi <jordieshier@gmail.com>
2022-12-12 02:40:43 +0000

    Dataset splitting

--- python/svm_classifier.py
+++ python/svm_classifier.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import numpy as np
+import torch
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -11,7 +12,6 @@ log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 
 # classifier-SVM hyperparameters and grid search
 kernel = "rbf"
-gpu_id = 0
 param_grid = {
     "C": [256, 128, 64, 32, 16, 8],
     "gamma": [2 ** (-12), 2 ** (-11), 2 ** (-10), 2 ** (-9), 2 ** (-8), 2 ** (-7)],
@@ -30,6 +30,37 @@ def load_dataset(input_file: str):
     return dataset
 
 
+def test_train_split(dataset):
+    # data split according to players + cross validation
+    torch.manual_seed(42)
+    player_id = dataset["player_ids"]
+    player_split = torch.randperm(len(np.unique(player_id))) + 1
+    player_split = player_split.numpy()
+
+    # train_split test_split player
+    train_split = []
+    test_split = []
+
+    train_split.append(player_split[0 : int(player_split.shape[0] * 0.8)])
+    test_split.append(
+        player_split[int(player_split.shape[0] * 0.8) : player_split.shape[0]]
+    )
+
+    train_split.append(player_split[2:10])
+    test_split.append(player_split[0:2])
+
+    train_split.append(np.hstack((player_split[4:10], player_split[0:2])))
+    test_split.append(player_split[2:4])
+
+    train_split.append(np.hstack((player_split[6:10], player_split[0:4])))
+    test_split.append(player_split[4:6])
+
+    train_split.append(np.hstack((player_split[8:10], player_split[0:6])))
+    test_split.append(player_split[6:8])
+
+    return train_split, test_split
+
+
 def main(arguments):
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
@@ -39,11 +70,17 @@ def main(arguments):
         help="Input dataset to be used for classification",
         type=str,
     )
+    parser.add_argument(
+        "--gpu",
+        default=None,
+        help="GPU to use for training, defaults to None",
+        type=str,
+    )
 
     args = parser.parse_args(arguments)
 
-    # Load the dataset
     dataset = load_dataset(args.input)
+    train_split, test_split = test_train_split(dataset)
 
 
 if __name__ == "__main__":
