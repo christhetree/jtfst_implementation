c31bc626baf434cd6f5f5cc514da57a86d14777a
christhetree <christhetree@gmail.com>
2022-12-14 14:15:07 +0000

    [cm] Refactoring and adding 1d reflection

--- python/scattering_1d.py
+++ python/scattering_1d.py
@@ -67,13 +67,14 @@ def calc_scat_transform_1d(x: T,
                            should_avg: bool = False,
                            highest_freq: Optional[float] = None,
                            avg_win: Optional[int] = None,
-                           squeeze_channels: bool = True) -> (T, List[float], List[T]):
+                           squeeze_channels: bool = True,
+                           reflect_t: bool = False) -> (T, List[float], List[T]):
     assert x.ndim == 3
     w = MorletWavelet.freq_to_w_at_s(1.0, s=1.0)
     mw = MorletWavelet(w=w, sr_t=sr)
-    wavelet_bank, freqs = make_wavelet_bank(mw, J, Q, highest_freq)
-    y = _calc_scat_transform_1d(x, sr, wavelet_bank, freqs, should_avg, avg_win, squeeze_channels)
-    return y, freqs, wavelet_bank
+    wavelet_bank, _, freqs_t, _ = make_wavelet_bank(mw, J, Q, highest_freq, reflect_t=reflect_t)
+    y = _calc_scat_transform_1d(x, sr, wavelet_bank, freqs_t, should_avg, avg_win, squeeze_channels)
+    return y, freqs_t, wavelet_bank
 
 
 def calc_scat_transform_1d_fast(x: T,
@@ -94,12 +95,14 @@ def calc_scat_transform_1d_fast(x: T,
             include_lowest_octave = False
 
         mw = MorletWavelet(w=w, sr_t=curr_sr)
-        wavelet_bank, freqs = make_wavelet_bank(
-            mw, n_octaves_t=1, steps_per_octave_t=Q, include_lowest_octave_t=include_lowest_octave)
+        wavelet_bank, _, freqs_t, _ = make_wavelet_bank(mw,
+                                                        n_octaves_t=1,
+                                                        steps_per_octave_t=Q,
+                                                        include_lowest_octave_t=include_lowest_octave)
         octave = calc_scalogram_1d(curr_x, wavelet_bank, take_modulus=True)
         octave = average_td(octave, curr_avg_win)
         octaves.append(octave)
-        freqs_all.extend(freqs)
+        freqs_all.extend(freqs_t)
         curr_x = average_td(curr_x, avg_win=2, hop_size=2)
         curr_sr /= 2
         curr_avg_win //= 2
@@ -120,17 +123,17 @@ def calc_scat_transform_1d_jagged(x: T,
     assert x.size(1) == len(freqs_x)
     w = MorletWavelet.freq_to_w_at_s(1.0, s=1.0)
     mw = MorletWavelet(w=w, sr_t=sr)
-    wavelet_bank, freqs = make_wavelet_bank(mw, J, Q, highest_freq)
+    wavelet_bank, _, freqs_t, _ = make_wavelet_bank(mw, J, Q, highest_freq)
     y_s = []
     freqs_out = []
-    for wavelet, freq in tqdm(zip(wavelet_bank, freqs)):
-        band_freqs = [f_x for f_x in freqs_x if f_x >= 2 * freq]
+    for wavelet, freq_t in tqdm(zip(wavelet_bank, freqs_t)):
+        band_freqs = [f_x for f_x in freqs_x if f_x >= 2 * freq_t]  # TODO(cm): check what condition is correct
         n_bands = len(band_freqs)
         if n_bands == 0:
             continue
         curr_x = x[:, :n_bands, :]
         curr_wb = [wavelet]
-        curr_freqs = [freq]
+        curr_freqs = [freq_t]
         y = _calc_scat_transform_1d(curr_x, sr, curr_wb, curr_freqs, should_avg, avg_win, squeeze_channels=False)
         if should_pad:
             pad_n = x.size(1) - n_bands
@@ -138,44 +141,28 @@ def calc_scat_transform_1d_jagged(x: T,
         y_s.append(y)
         # plt.imshow(y.squeeze(0).squeeze(0).numpy(), aspect="auto")
         # plt.show()
-        freqs_out.append(freq)
+        freqs_out.append(freq_t)
 
     return y_s, freqs_out
 
 
 if __name__ == "__main__":
-    should_avg_1 = False
-    should_avg_f = False
-    should_avg_t = False
-    log.info(f"should_avg_1 = {should_avg_1}")
-    log.info(f"should_avg_f = {should_avg_f}")
-    log.info(f"should_avg_t = {should_avg_t}")
-
-    # start_n = int(4 * 48000)
-    # n_samples = 2 * 48000
     start_n = 0
     n_samples = 2 ** 16
-
-    audio_path = "../data/sine_sweep.wav"
-    chirp_audio, sr = torchaudio.load(audio_path)
-    chirp_audio = chirp_audio[:, start_n:start_n + n_samples]
-    chirp_audio = chirp_audio.view(1, 1, -1)
+    # n_samples = 4096
 
     audio_path = "../data/flute.wav"
     flute_audio, sr = torchaudio.load(audio_path)
     flute_audio = flute_audio[:, start_n:n_samples]
     flute_audio = tr.mean(flute_audio, dim=0)
-    flute_audio = flute_audio.view(1, 1, -1)
 
-    # audio = chirp_audio
     audio = flute_audio
 
-    # n_samples = 4096
-    # sr = 48000
-    # audio_1 = make_pure_sine(n_samples, sr, freq=4000, amp=1.0)
-    # audio_2 = make_pulse(n_samples, center_loc=0.5, dur_samples=16, amp=4.0)
-    # audio_3 = make_exp_chirp(n_samples, sr, start_freq=750, end_freq=20000, amp=1.0)
-    # audio = audio_1 + audio_2 + audio_3
+    sr = 48000
+    audio_1 = make_pure_sine(n_samples, sr, freq=4000, amp=1.0)
+    audio_2 = make_pulse(n_samples, center_loc=0.5, dur_samples=128, amp=4.0)
+    audio_3 = make_exp_chirp(n_samples, sr, start_freq=20, end_freq=20000, amp=1.0)
+    audio = audio_1 + audio_2 + audio_3
 
     audio = audio.view(1, 1, -1)
 
@@ -189,9 +176,7 @@ if __name__ == "__main__":
                                                             sr,
                                                             J_1,
                                                             Q_1,
-                                                            should_avg_1,
-                                                            highest_freq=highest_freq,
-                                                            avg_win=None)
+                                                            highest_freq=highest_freq)
 
     log.info(f"scalogram shape = {scalogram.shape}")
     # log.info(f"scalogram mean = {tr.mean(scalogram)}")
@@ -199,12 +184,10 @@ if __name__ == "__main__":
     # log.info(f"scalogram max = {tr.max(scalogram)}")
     # log.info(f"scalogram min = {tr.min(scalogram)}")
     log.info(f"scalogram energy = {MorletWavelet.calc_energy(scalogram)}")
-
     mean = tr.mean(scalogram)
     std = tr.std(scalogram)
     scalogram_to_plot = tr.clip(scalogram, mean - (4 * std), mean + (4 * std))
-
-    plot_scalogram_1d(scalogram_to_plot[0], title="scalo", dt=1.0 / sr, freqs=freqs, n_y_ticks=J_1)
+    plot_scalogram_1d(scalogram_to_plot[0], title="scalo", dt=None, freqs=freqs, n_y_ticks=J_1)
 
     # scalogram_fast, freqs_fast = calc_scat_transform_1d_fast(audio, sr, J_1, Q_1)
     # log.info(f"scalogram_fast shape = {scalogram_fast.shape}")
@@ -213,17 +196,16 @@ if __name__ == "__main__":
     # # log.info(f"scalogram_fast max = {tr.max(scalogram_fast)}")
     # # log.info(f"scalogram_fast min = {tr.min(scalogram_fast)}")
     # log.info(f"scalogram_fast energy = {MorletWavelet.calc_energy(scalogram_fast)}")
-    # plot_scalogram_1d(scalogram_fast[0], title="scalo fast", dt=1.0 / sr, freqs=freqs_fast)
+    # plot_scalogram_1d(scalogram_fast[0], title="scalo fast", dt=None, freqs_t=freqs_fast)
     # exit()
 
-    J_2_f = 2
-    Q_2_f = 2
-    highest_freq_f = None
-    # highest_freq_f = 12000
     J_2_t = 12
-    Q_2_t = 2
+    Q_2_t = 1
     # highest_freq_t = None
     highest_freq_t = 12000
+
+    should_avg_t = False
+    log.info(f"should_avg_t = {should_avg_t}")
     avg_win_f = None
     avg_win_t = None
 
@@ -237,4 +219,9 @@ if __name__ == "__main__":
                                                  avg_win=avg_win_t,
                                                  should_pad=True)
     log.info(f"freqs_t = {freqs_t}")
+    for y, freq in zip(y_t, freqs_t):
+        y = y.squeeze(0).squeeze(0).numpy()
+        plt.imshow(y, aspect="auto", interpolation="none", cmap="OrRd")
+        plt.title(f"2nd order time scattering, freq = {freq:.2f}")
+        plt.show()
     exit()
